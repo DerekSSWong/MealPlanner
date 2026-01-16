6c858249f47a97a02c2c27100b5592c3eb421604
DerekSSWong <ssderekwong@gmail.com>
2026-01-16 09:21:03 +0000

    Tests for ingredients

--- ingredient.py
+++ ingredient.py
@@ -1,44 +1,55 @@
 class Ingredient:
     """Represents one portion of ingredient. Contains the name and description of the given ingredient.
 
+    Attributes:
+        name (str): The name of the ingredient, must be a non-empty string
+        desc (str): A brief description of the ingredient
+
     Example:
         The Ingredient class is contructed like so:
         ::
             $ Ingredient("steak", "ideally ribeye")
     """
+
+    NAME_MAX_LENGTH = 32
+    DESCRIPTION_MAX_LENGTH = 128
+
     def __init__(self,  name:str, desc:str) -> None:
         self.name = name
         self.desc = desc
     
     @property
     def name(self):
-        """Retrieves the name of the ingredient
-
-        Returns:
-            str
-        """
         return self._name
     
     @name.setter
     def name(self, value):
-        self._name = str(value)
+        #First check if string
+        if not isinstance(value, str):
+            raise TypeError(f"Ingredient name must be a string, not {type(value).__name__}")
+        
+        #Check if string length is between 1 and max length
+        _stripped_value = value.strip()
+        if len(_stripped_value) == 0:
+            raise ValueError("The name of the ingredient cannot be blank")
+        elif len(_stripped_value) > self.NAME_MAX_LENGTH:
+            raise ValueError("Ingredient name cannot exceed 32 characters")
+        
+        self._name = _stripped_value
     
     @property
     def desc(self):
-        """Retrieves the description of the ingredient
-
-        Returns:
-            str
-        """
         return self._desc
     
     @desc.setter
     def desc(self, value):
-        self._desc = value
+        _stripped_value = str(value).strip()
+
+        #Check if string exceeds length
+        if len(_stripped_value) > self.DESCRIPTION_MAX_LENGTH:
+            raise ValueError("Ingredient description cannot exceed 128 characters")
+
+        self._desc = _stripped_value
 
     def __str__(self) -> str:
         return str(self.ingredient_dict)
-    
-
-test = Ingredient("testname", "desc")
-print(test.name)
--- test_ingredient.py
+++ test_ingredient.py
@@ -1,10 +1,37 @@
-
+import pytest
 from ingredient import Ingredient
 
-test_ingredient = Ingredient("Prawn", "Frozen is fine")
+
 
 def test_get_name():
+    test_ingredient = Ingredient("Prawn", "Frozen is fine")
     assert test_ingredient.name == "Prawn"
 
+def test_empty_name():
+    with pytest.raises(ValueError) as exc_info:
+        Ingredient("", "No space")
+    assert "The name of the ingredient cannot be blank" in str(exc_info.value)
+
+def test_space_name():
+    with pytest.raises(ValueError) as exc_info:
+        Ingredient(" ", "One space")
+    assert "The name of the ingredient cannot be blank" in str(exc_info.value)
+
+def test_long_name():
+    with pytest.raises(ValueError) as exc_info:
+        Ingredient('a' * 33, "one two three")
+    assert "Ingredient name cannot exceed 32 characters" in str(exc_info.value)
+
+def test_number_name():
+    with pytest.raises(TypeError) as exc_info:
+        Ingredient(123, "one two three")
+    assert "Ingredient name must be a string" in str(exc_info.value)
+    
 def test_get_desc():
+    test_ingredient = Ingredient("Prawn", "Frozen is fine")
     assert test_ingredient.desc == "Frozen is fine"
+
+def test_long_desc():
+    with pytest.raises(ValueError) as exc_info:
+        Ingredient("big name", 'a' * 129)
+    assert "Ingredient description cannot exceed 128 characters" in str(exc_info.value)
